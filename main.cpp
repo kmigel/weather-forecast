b2f234d818bc5de9f7e2f72cce49d8cc6a1d6825
kmigel <annkri2428@gmail.com>
2023-05-29 21:29:15 +0200

    Add city default value

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]) {
     CURL *curl;
     CURLcode res;
     
-    string city;
+    string city = "London";
     if(argc != 1 && string(argv[1])[0] != '-') {
         city = argv[1];
     }
